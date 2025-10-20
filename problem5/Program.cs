eaad590cd9173d6fe1a1a42436652327d7b477fd
Samson <david.samson@stellasource.com>
2025-10-20 15:35:36 -0500

    added solutions for problems 5 and 6

---
+++ problem5/Program.cs
@@ -0,0 +1,28 @@
+﻿while (true)
+{
+    var input = Console.ReadLine();
+    var validInput = int.TryParse(input, out int inputNumber);
+    
+    if (!validInput) continue;
+    
+    Console.WriteLine(GetSmallestPerfectRemainder(inputNumber));
+}
+
+int GetSmallestPerfectRemainder(int number)
+{
+    int smallestPerfectRemainderSource = 1;
+    
+    int i = number;
+    while (i > 0)
+    {
+        if (smallestPerfectRemainderSource % i > 0)
+        {
+            smallestPerfectRemainderSource++;
+            i = number;
+        }
+        
+        i--;
+    }
+    
+    return smallestPerfectRemainderSource;
+}
---
+++ problem6/Program.cs
@@ -0,0 +1,38 @@
+﻿while (true)
+{
+    var input = Console.ReadLine();
+    var validInput = int.TryParse(input, out int inputNumber);
+    
+    if (!validInput) continue;
+    
+    int sumOfSquares = GetSumOfSquaresUpTo(inputNumber);
+    int squareOfSums = GetSquareOfSumsUpTo(inputNumber);
+    
+    Console.WriteLine(Math.Abs(sumOfSquares - squareOfSums));
+}
+
+int GetSumOfSquaresUpTo(int inputNumber)
+{
+    int sum = 0;
+    int i = inputNumber;
+    while (i > 0)
+    {
+        sum += i * i;
+        i--;
+    }
+
+    return sum;
+}
+
+int GetSquareOfSumsUpTo(int inputNumber)
+{
+    int sum = 0;
+    int i = inputNumber;
+    while (i > 0)
+    {
+        sum += i;
+        i--;
+    }
+
+    return sum * sum;
+}
