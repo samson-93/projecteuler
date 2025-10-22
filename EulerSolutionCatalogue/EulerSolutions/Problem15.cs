9d987d4e98a47c3a264ea2e8472c9e527908e229
Samson <david.samson@stellasource.com>
2025-10-22 15:56:10 -0500

    fixed problem 15, replaces costly dfs solution with object approach for simple O(n) binomial coefficient approach -- gpt reviewed my code for this one

--- EulerSolutionCatalogue/EulerSolutions/Problem15.cs
+++ EulerSolutionCatalogue/EulerSolutions/Problem15.cs
@@ -9,118 +9,26 @@ public class Problem15 : IEulerSolution
         Console.WriteLine("Enter Grid Size (n where size = n x n): ");
         var gridSize = Convert.ToInt32(Console.ReadLine());
         
-        var uniqueRoutesFound = FindRoutes(gridSize);
+        var uniqueRoutesFound = FindUniqueRoutes(gridSize);
         Console.WriteLine("Found " + uniqueRoutesFound + " unique routes from top-left of grid to bottom-right of grid.");
     }
 
-    private long FindRoutes(int gridSize)
+    private long FindUniqueRoutes(long gridSize)
     {
-        long uniqueRoutes = 0;
-        
-        Grid grid = new Grid(gridSize);
-        // grid.Print();
-        
-        var rootVector = grid.Vectors[0, 0];
-        Route(rootVector);
-
-        void Route(Vector v)
-        {
-            if (v.HasDownVector)
-            {
-                Route(v.DownVector!);
-            }
-
-            if (v.HasRightVector)
-            {
-                Route(v.RightVector!);
-            }
-            
-            if (v is { HasDownVector: false, HasRightVector: false })
-            {
-                uniqueRoutes++;
-            }
-        }
-        
-        return uniqueRoutes;
+        return GetBinomialCoefficient((2 * gridSize), gridSize);
     }
 
-    class Grid
+    // gpt had to give me a reeducation here :<
+    private long GetBinomialCoefficient(long n, long k)
     {
-        public Vector[,] Vectors;
+        long aggregate = 1;
+
+        for (long i = 0; i < k; i++)
+        {
+            aggregate *= n - i;
+            aggregate /= i + 1;
+        }
         
-        public Grid(int gridSize)
-        {
-            char key = 'A';
-            int dimensionLimit = (gridSize + 1);
-            
-            Vectors = new Vector[dimensionLimit, dimensionLimit];
-            
-            for (int i = 0; i < dimensionLimit; i++)
-            {
-                for (int j = 0; j < dimensionLimit; j++)
-                {
-                    int x = -gridSize + i;
-                    int y = gridSize - j;
-
-                    var vector = new Vector(x, y, key);
-                    key = (char)(key + 1);
-
-                    if (i + 1 < dimensionLimit)
-                    {
-                        vector.HasDownVector = true;
-                    }
-
-                    if (j + 1 < dimensionLimit)
-                    {
-                        vector.HasRightVector = true;
-                    }
-
-                    if (i > 0)
-                    {
-                        Vectors[i - 1, j].DownVector = vector;
-                    }
-
-                    if (j > 0)
-                    {
-                        Vectors[i, j - 1].RightVector = vector;
-                    }
-                    
-                    Vectors[i, j] = vector;
-                }
-            }
-        }
-
-        public void Print()
-        {
-            for (int i = 0; i < Vectors.GetLength(0); i++)
-            {
-                for (int j = 0; j < Vectors.GetLength(1); j++)
-                {
-                    var currentVector = Vectors[i, j];
-
-                    string output = "";
-                    // output = currentVector.LeftVectorId != null ? "<" : "";
-                    // output += currentVector.UpVectorId != null ? "^" : "";
-                    output += currentVector.Id.ToString();
-                    // output += currentVector.HasDownVector ? "_" : "";
-                    // output += currentVector.HasRightVector ? ">" : "";
-                
-                    Console.Write(output + '\t');
-                }
-            
-                Console.Write('\n');
-            }
-        }
-    }
-    
-    class Vector(int x, int y, char id)
-    {
-        public char Id = id;
-        public int X = x;
-        public int Y = y;
-        public bool HasDownVector = false;
-        public bool HasRightVector = false;
-        public Vector? DownVector = null;
-        public Vector? RightVector = null;
+        return aggregate;
     }
 }
